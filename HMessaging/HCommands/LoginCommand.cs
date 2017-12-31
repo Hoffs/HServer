53c5495cdc8ab540cde6d5ca0e026a12146fed0d
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2017-12-31 20:39:30 +0200

    Added checking if user is already authenticated and moved out message sending to methods.

--- HMessaging/HCommands/LoginCommand.cs
+++ HMessaging/HCommands/LoginCommand.cs
@@ -10,30 +10,46 @@ namespace CoreServer.HMessaging.HCommands
     {
         public async Task Execute(RequestMessage message, HClient client)
         {
+            if (client.Authenticated)
+            {
+                await SendErrorMessageTask(client);
+                return;
+            }
+
             var loginRequest = LoginMessageRequest.Parser.ParseFrom(message.Message);
             var result = await client.TryAuthenticatingTask(loginRequest.Username, loginRequest.Password, loginRequest.Token);
             Console.WriteLine("[SERVER] After login for client {0}: {1} {2}", client.GetDisplayName(), result.Item1, result.Item2);
             if (client.Authenticated)
             {
-                await client.SendMessageToUserTask(new ResponseMessage
-                {
-                    Status = ResponseStatus.Success,
-                    Type = RequestType.Login,
-                    Message = new LoginMessageResponse
-                    {
-                        Token = client.Token,
-                        UserId = client.Id
-                    }.ToByteString()
-                });
+                await SendSuccessMessageTask(client);
             }
             else
             {
-                await client.SendMessageToUserTask(new ResponseMessage
-                {
-                    Status = ResponseStatus.Error,
-                    Type = RequestType.Login
-                });
+                await SendErrorMessageTask(client);
             }
+        }
+
+        private static async Task SendErrorMessageTask(HClient client)
+        {
+            await client.SendMessageToUserTask(new ResponseMessage
+            {
+                Status = ResponseStatus.Error,
+                Type = RequestType.Login
+            });
+        }
+
+        private static async Task SendSuccessMessageTask(HClient client)
+        {
+            await client.SendMessageToUserTask(new ResponseMessage
+            {
+                Status = ResponseStatus.Success,
+                Type = RequestType.Login,
+                Message = new LoginMessageResponse
+                {
+                    Token = client.Token,
+                    UserId = client.Id
+                }.ToByteString()
+            });
         }
     }
 }
